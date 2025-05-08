199cf06fe6417b5e94300b9a44ea9812a9d3db20
Manas Sethi <venummanas@gmail.com>
2025-05-08 21:20:20 +0530

    added secrets_sample

--- lib/secrets_sample.dart
+++ lib/secrets_sample.dart
@@ -1,21 +1,21 @@
 // secrets.dart
 
-const String FCM_SERVER_KEY = "eAEw4YHcTD2C8xMAjHGkvB:APA91bG1hOw0C4KbO30LTgQul_zQJfhaIX3ieYnxRXmUdBtEKaZmmV-mm4gLauZUVp72MEEOoF0R9B5wLe-wKWhQvvKK8nTExEVwPRlrHrXJ-bodpeXxiJA";
+const String FCM_SERVER_KEY = "YOUR_CREDENTIALS";
 
-const String ONESIGNAL_INITIALISE_ID = "c75785ad-be27-4b79-add2-6133abf2dde8";
+const String ONESIGNAL_INITIALISE_ID = "YOUR_CREDENTIALS";
 
-const String GEMINI_API_KEY = "AIzaSyDJvfen7-_lvlP5rd2vHaz9Xh0ubGshPtg";
+const String GEMINI_API_KEY = "OUR_CREDENTIALS";
 
-const String RZP_API_KEY = "rzp_live_k8RwuEuNB3PAhq";
+const String RZP_API_KEY = "YOUR_CREDENTIALS";
 
-const String ONESIGNAL_API_KEY = "os_v2_app_y5lylln6e5fxtlosmez2x4w55a3cnpxahixuhb5mmoz3s4trd4ww7v3scyvhmtt36dbyytp4j5nksdynpezjopazlk77eqjd4pnvnha";
+const String ONESIGNAL_API_KEY = "YOUR_CREDENTIALS";
 
-const String ZEGOCLOUD_APP_ID = "1185214722";
+const String ZEGOCLOUD_APP_ID = "YOUR_CREDENTIALS";
 
-const String ZEGOCLOUD_APP_SIGN = "e4a0ad77f57838937d73e4fb38b888a2fbc1e64e837d47806048b3b860f03f24";
+const String ZEGOCLOUD_APP_SIGN = "YOUR_CREDENTIALS";
 
-const String credentials = "cnpwX3Rlc3RfMjBqWkF0NXNOeFljaWc6UG9aeGVsMFQzNzkyalNqNkx3N0d5VGMx";
+const String credentials = "YOUR_CREDENTIALS";
 
-const String key = "rzp_live_k8RwuEuNB3PAhq";
+const String key = "YOUR_CREDENTIALS";
 
-const String secret = "4aZJtJVqJucwnvZmwTSWshq9";
+const String secret = "YOUR_CREDENTIALS";
