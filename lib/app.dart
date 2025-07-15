7633bd866774ee68186f17b11d408ebe37c7b975
Manas Sethi <venummanas@gmail.com>
2025-07-15 18:42:45 +0530

    added another function

--- lib/app.dart
+++ lib/app.dart
@@ -2,10 +2,13 @@
 
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:get/get_navigation/src/routes/get_route.dart';
 import 'package:gibud/bindings/general_bindings.dart';
 import 'package:gibud/splash_screen.dart';
 
 import 'chat/individual_chat_page.dart';
+import 'features/screens/home/home_page.dart';
+import 'features/screens/tongue_analysis/widgets/TongueAnalysisResultPage.dart';
 
 
 class MyApp extends StatefulWidget {
@@ -24,6 +27,11 @@ class _MyAppState extends State<MyApp> {
 
   Widget build(BuildContext context) {
     return GetMaterialApp(
+      getPages: [
+        GetPage(name: '/', page: () => HomePage()),
+        // add this line 👇👇👇
+        GetPage(name: '/result', page: () => TongueAnalysisResultPage()),
+      ],
       navigatorObservers: [AppNavigatorObserver()],
       debugShowCheckedModeBanner: false,
       initialBinding: GeneralBindings(),
